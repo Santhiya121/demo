6ed3631627fa90d68dbc4dffb6e64cdd5057ba9c
Santhiya121 <santhiyar121@gmail.com>
2025-05-19 16:47:36 +0530

    command

---
+++ script.js
@@ -0,0 +1,17 @@
+/*console.log("hiii frndzz")
+console.log("hiii frndzz")*/
+// console.log("hiii frndzz")
+// let a = 10
+// a = 20
+// console.log(a)
+// const b = 30
+// console.log(b)
+// b =40
+// console.log(b)
+
+
+const car = {type:"Fiat", model:"500", color:"white"};
+console.log(car)
+
+const d = new Date()
+console.log(d)
